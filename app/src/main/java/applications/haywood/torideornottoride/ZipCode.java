5810e8714ec816181c408a6a665240d9e4d792f7
Allan H <allanhaywood@dslextreme.com>
2015-05-04 10:04:20 -0700

    in progress

---
+++ app/src/main/java/applications/haywood/torideornottoride/ZipCode.java
@@ -0,0 +1,20 @@
+package applications.haywood.torideornottoride;
+
+/**
+ * Created by Allan on 5/4/2015.
+ */
+public class ZipCode {
+
+    static final String  zipCodePattern = "\\d{5}(-\\d{4})?";
+
+    private int zipCode;
+
+    public void setZipCodeWithInt(String zipcode)
+    {
+        zipcode.matches(ZipCode.)
+        this.zipCode = Integer.parseInt(zipcode);
+    }
+
+
+
+}
---
+++ app/src/main/java/applications/haywood/torideornottoride/ZipCodeWeather.java
@@ -0,0 +1,10 @@
+package applications.haywood.torideornottoride;
+
+/**
+ * Created by Allan on 5/4/2015.
+ */
+public class ZipCodeWeather {
+
+
+
+}

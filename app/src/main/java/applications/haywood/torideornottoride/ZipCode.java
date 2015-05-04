1468224a412655a28e9116d5138a4533a9e8eebf
Allan H <allanhaywood@dslextreme.com>
2015-05-04 10:07:45 -0700

    in progress

--- app/src/main/java/applications/haywood/torideornottoride/ZipCode.java
+++ app/src/main/java/applications/haywood/torideornottoride/ZipCode.java
@@ -5,13 +5,13 @@ package applications.haywood.torideornottoride;
  */
 public class ZipCode {
 
-    static final String  zipCodePattern = "\\d{5}(-\\d{4})?";
+    static final String  ZipCodePattern = "\\d{5}(-\\d{4})?";
 
     private int zipCode;
 
     public void setZipCodeWithInt(String zipcode)
     {
-        zipcode.matches(ZipCode.)
+        zipcode.matches(ZipCode.ZipCodePattern);
         this.zipCode = Integer.parseInt(zipcode);
     }
 
